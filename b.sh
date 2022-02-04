45fe034000d1e9aaa3520b4a9bead8e8e03c6073
Kenneth Zhao <kzhao@solution-soft.com>
2022-02-04 00:53:44 -0800

    upgrade to release 31

---
+++ b.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+TAG="build-v18.03r30"
+
+set -ex
+
+docker build --pull --rm --compress -t solutionsoft/timemachine-sidecar:latest -t solutionsoft/timemachine-sidecar:${TAG} .
---
+++ p.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+TAG=build-v18.03r30
+
+set -ex
+
+docker push solutionsoft/timemachine-sidecar:latest
+docker push solutionsoft/timemachine-sidecar:${TAG}
